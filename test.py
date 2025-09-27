88e4ded6495245016ba8b9e6ac5c4e465f27955e
Lucas Jin <kblazer20@gmail.com>
2025-09-27 14:07:34 -0400

    Changed testing to by episode and added video recording

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
+from gymnasium.wrappers import RecordVideo
 
-env = gym.make("LunarLander-v3", render_mode="human")
+env = gym.make("LunarLander-v3", render_mode="rgb_array") # Use human render mode to visualize, rgb to save video
+env = RecordVideo(env, video_folder="videos/", name_prefix="lander_run")
 
 model = PPO.load("ppo-LunarLander-v3")
 
-obs, info = env.reset()
-for _ in range(1000):
-    action, _states = model.predict(obs)
-    obs, reward, terminated, truncated, info = env.step(action)
-    if terminated or truncated:
-        obs, info = env.reset()
+for episode in range(5):
+    obs, info = env.reset()
+    done = False
+    while not done:
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated
 
 env.close()
